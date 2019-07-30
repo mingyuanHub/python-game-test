981d344b308312191e3d38670977c4a1fb8b2a26
mingyuan <mamingyuan@lanlingdai.net>
2019-07-30 18:28:37 +0800

    优化

--- af/main.py
+++ af/main.py
@@ -2,7 +2,7 @@ import unittest, HtmlTestRunner
 
 def getAfSuite():
     suite = unittest.TestSuite()
-    all_cases = unittest.defaultTestLoader.discover('/Users/mingyuan/python/game-test/af/', pattern='test*.py')
+    all_cases = unittest.defaultTestLoader.discover('/Users/mingyuan/python/python-game-test/af/', pattern='test*.py')
 
     for case in all_cases:
         suite.addTests(case)
@@ -10,6 +10,6 @@ def getAfSuite():
         
 if __name__ == '__main__':
     suite = getAfSuite()
-    testRunner=HtmlTestRunner.HTMLTestRunner(output='./game-test/report', template='./game-test/report/index.html', combine_reports=True, report_name="game-test-report",  add_timestamp=False)
+    testRunner=HtmlTestRunner.HTMLTestRunner(output='./python-game-test/report', template='./python-game-test/report/index.html', combine_reports=True, report_name="game-test-report",  add_timestamp=False)
     testRunner.run(suite)
 
